a61879c46fff5734380c8ab313c3750d3eb797fe
Ritikjainkkar <ritikjainkkar@gmail.com>
2021-07-25 14:16:26 +0530

    cards updated

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,21 +4,26 @@ import './styles.css';
 import{addPost as addPostAction} from '../../actions/actions';
 import {IPosts} from '../Posts/interfaces';
 import {IForm} from './interfaces';
-const Form = ({onSubmitClick}: IForm) => {
+const Form = ({ onSubmitClick }: IForm) => {
 
   const [heading, setHeading] = useState('');
   const [subHeading, setSubHeading] = useState('');
   const [summary, setSummary] = useState('');
 
   const onSubmit = () => {
-    onSubmitClick({heading, subHeading, summary})
+    if( heading !== "" && subHeading !== "" && summary !== "" ) {
+      onSubmitClick({heading, subHeading, summary})
+      setHeading('');
+      setSubHeading('');
+      setSummary('');
+    }
   }
   return (
     <React.Fragment>
       <form id="msform">
         <fieldset>
           <h2 className="fs-title">Create Post</h2>
-          <h3 className="fs-subtitle">This is step 1</h3>
+          <h3 className="fs-subtitle">Fill the all mention detail to submit the form</h3>
           <input type="text" name="heading" value={heading} onChange={(e) => setHeading(e.target.value)} placeholder="Heading" />
           <input type="text" name="pass" value={subHeading} onChange={(e) => setSubHeading(e.target.value)} placeholder="Sub-Heading" />
           <textarea name="cpass" value={summary} onChange={(e) => setSummary(e.target.value)} placeholder="Summary" />
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -5,20 +5,24 @@ import {IPostProps}  from './interfaces'
 import {IState} from './../../reducer/interfaces';
 
 const Post = ({posts}: IPostProps) => {
-  return (<React.Fragment> {posts.map((post, index)=>(
-    <ul className="cards" key={index}>
-    <li className="cards__item">
-      <div className="card">
-        <div className="card__image card__image--flowers"></div>
-        <div className="card__content">
-           <div className="card__title">{post.heading}</div>
-          <div className="card__title">{post.subHeading}</div>
-          <p className="card__text">{post.summary}</p>
-        </div>
-      </div>
-    </li>
-</ul>
-  ))} </React.Fragment>);
+  return (
+  <React.Fragment> 
+      <ul className="cards" >
+        {
+          posts.map( (post, index)=>(
+            <li key={index} className="cards__item">
+              <div className="card">
+                <div className="card__content">
+                  <div className="card__title">{post.heading}</div>
+                  <div className="card__sub__title">{post.subHeading}</div>
+                  <p className="card__text">{post.summary}</p>
+                </div>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+  </React.Fragment>);
 }
 
 const mapStateToProps = (state:IState ) => {
